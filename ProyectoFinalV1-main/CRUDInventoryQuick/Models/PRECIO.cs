c92b8e3dc8ceffad8a2cd7b4d9bd085c6969fdf0
kevin.moreno249 <kevin.moreno249@misena.edu.co>
2023-02-11 23:52:00 -0500

    Traduccion Perfil y Correo

--- ProyectoFinalV1-main/CRUDInventoryQuick/Models/PRECIO.cs
+++ ProyectoFinalV1-main/CRUDInventoryQuick/Models/PRECIO.cs
@@ -19,26 +19,31 @@ namespace CRUDInventoryQuick.Models
         /// Indica la fecha ingreso del precio
         /// </summary>
         //[BindProperty, DataType(DataType.Date)]
+        [Required(ErrorMessage ="El campo Fecha de Ingreso no puede estar vacio")]
         [Column(TypeName = "datetime")]
         public DateTime FechaIngreso { get; set; }
         /// <summary>
         /// Indica el precio compra del producto
         /// </summary>
+        [Required(ErrorMessage = "El campo Precio de Compra no puede estar vacio")]
         [Column(TypeName = "decimal(10,0)")]
         public decimal PrecioCompra { get; set; }
         /// <summary>
         /// Indica un posible descuento que el producto pueda poseer
         /// </summary>
+        [Required(ErrorMessage = "El campo Descuento no puede estar vacio")]
         [Column(TypeName = "decimal(10,0)")]
         public decimal Descuento { get; set; }
         /// <summary>
         /// Indica el precio venta incial 
         /// </summary>
+        [Required(ErrorMessage = "El campo Precio Venta Inicial no puede estar vacio")]
         [Column(TypeName = "decimal(10,0)")]
         public decimal PrecioVentaInicial { get; set; }
         /// <summary>
         /// Indica el precio venta final
         /// </summary>
+        [Required(ErrorMessage = "El campo Precio venta Final no puede estar vacio")]
         [Column(TypeName = "decimal(10,0)")]
         public decimal PrecioVentaFinal { get; set; }
         /// <summary>
